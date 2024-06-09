c817cbb421a87245ad8c3d9ebbf564f2f4493029
ARSHAD BAGWAN <bagwanarshad180@gmail.com>
2024-06-09 17:24:02 +0530

    read file from command line

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.*;
 public class Main {
     public static void main(String[] args) {
+        File fs = null;
+        if( 0 < args.length)
+        {
+            fs = new File(args[0]);
+
+        }
+        else {
+            System.err.println("Invalid arguments count:" + args.length);
+
+        }
         System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        String userinput = sc.nextLine();
+        String command = userinput.split(" ")[1];
+        System.out.println("command is " + command);
+
+
     }
 }
